ec40fd8ed05e5c2ee3b55751d1093daad7cd7227
William Owen <william.owen.bsc@gmail.com>
2024-05-18 07:42:31 -0700

    Rm test code

--- src/components/overview/figures/lead-map/LeadMap.tsx
+++ src/components/overview/figures/lead-map/LeadMap.tsx
@@ -6,26 +6,13 @@ import { useEffect, useRef } from 'react'
 import renderMap from './renderMap'
 
 export default function LeadMap(props) {
-  //   console.log(d3)
-  //   console.log({ statesGeoJson })
   const content = useRef<SVGSVGElement>(null)
 
   useEffect(() => {
     console.log('test', content.current)
     if (content.current) {
       renderMap()
-      //   const map = d3.select('svg')
-
-      //   map.append('text').text('Hellloooo').attr('x', 123)
-
-      //   console.log({ map })
-
-      //   content.current.append()
     }
-
-    // svg.append('text').text('hi').attr('x', 20)
-    // console.log({ svg })
-    // renderMap(content.current)u
   }, [content.current])
 
   return (
